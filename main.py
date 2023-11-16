3736dc046711979f9b2f84e30e095c39ffbacf4a
bilel233 <bilel.mjdb@gmail.com>
2023-11-16 10:06:23 +0100

    ajout d'une music dans le repertoire Desktop

--- main.py
+++ main.py
@@ -5,5 +5,17 @@
 
 ############################################
 
+import script           # import du module script
+
+print("==================================")
+
+script.download_video("https://www.youtube.com/watch?v=5TDxf4QXDlc&ab_channel=oneek1","./SUPINFO")
+
+print("==================================")
+
+
+
+
+
 
 
